dc8ef8a7ab091a153fd982c55b7fc0632d8885ec
srfrankie <frank_toledo_009@hotmail.com>
2017-05-03 18:58:46 -0300

    se agrego proveedor

---
+++ src/Modelo/Proveedor.java
@@ -0,0 +1,29 @@
+package Modelo;
+
+public class Proveedor {
+	private int id;
+	private String nombre;
+	
+	public Proveedor(){
+	}
+	
+	public Proveedor(String nombre) {
+		super();
+		this.nombre = nombre;
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+}
